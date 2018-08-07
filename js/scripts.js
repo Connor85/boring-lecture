59394e9e470192ba6b58821253967fb245115471
Guest User <Guest@epicodus-118.local>
2018-08-07 11:28:42 -0700

    add Javascript

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+//   $("#p3").click(function(){
+//     $("#p3").removeClass();
+//     $("#p3").addClass("essay-body");
+//   });
+// });
+
+$(document).ready(function(){
+  $("#p3").click(function(){
+    $("#p3").toggleClass("essay-body");
+  });
+
+    $("#p4").click(function(){
+    $("#p4").toggleClass("essay-body");
+  });
+
+});
